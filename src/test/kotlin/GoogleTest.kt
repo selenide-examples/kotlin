deecdf748c3ef399361757c8357b4de334c42cd0
aTan <atan.aka.xellos@gmail.com>
2019-03-12 18:00:10 +0200

    update kotlin up to 1.3.21 update selenide up to 5.2.0 update junit up to 5.4.0 (from junit 4)

--- src/test/kotlin/GoogleTest.kt
+++ src/test/kotlin/GoogleTest.kt
@@ -1,17 +1,17 @@
-import com.codeborne.selenide.CollectionCondition.size
 import com.codeborne.selenide.CollectionCondition.sizeGreaterThan
 import com.codeborne.selenide.Condition.text
 import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.ElementsCollection
-import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.Selenide.open
-import com.codeborne.selenide.Selenide.getElement
-import com.codeborne.selenide.Selenide.getElements
 import com.codeborne.selenide.Selenide.`$`
 import com.codeborne.selenide.Selenide.`$$`
+import com.codeborne.selenide.Selenide.element
+import com.codeborne.selenide.Selenide.elements
 import com.codeborne.selenide.SelenideElement
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.BeforeAll
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.openqa.selenium.By
 
 /*
@@ -20,36 +20,38 @@ import org.openqa.selenium.By
  * 1. Selenide.$(By.name("q")); // $ doesn't compile in Kotlin :(
  * 2. Selenide.getElement(By.name("q")).val("selenide+kotlin"); // val doesn't compile in Kotlin :(
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GoogleTest {
 
-    @Before fun setUp() {
-        Configuration.startMaximized = false;
+    @BeforeAll fun setUp() {
+        Configuration.startMaximized = false
         open("https://google.com/ncr")
     }
-    
-    @Test fun usingDollarsWithBackticks() {
+
+    @Test
+    fun usingDollarsWithBackticks() {
         `$`(By.name("q")).setValue("selenide").pressEnter()
         `$$`("#ires .g").shouldHave(sizeGreaterThan(5))
-        `$`("#ires .g").shouldHave(text("concise ui tests in Java"));
+        `$`("#ires .g").shouldHave(text("concise ui tests in Java"))
     }
 
     @Test fun notUsingDollars() {
-        getElement(By.name("q")).setValue("selenide").pressEnter()
-        getElements(By.cssSelector("#ires .g")).shouldHave(sizeGreaterThan(5))
-        getElement(By.cssSelector("#ires .g")).shouldHave(text("concise ui tests in Java"));
+        element(By.name("q")).setValue("selenide").pressEnter()
+        elements(By.cssSelector("#ires .g")).shouldHave(sizeGreaterThan(5))
+        element(By.cssSelector("#ires .g")).shouldHave(text("concise ui tests in Java"))
     }
 
     @Test fun usingAliases() {
         get("[name=q]").setValue("selenide").pressEnter()
         all("#ires .g").shouldHave(sizeGreaterThan(5))
-        get("#ires .g").shouldHave(text("concise ui tests in Java"));
+        get("#ires .g").shouldHave(text("concise ui tests in Java"))
     }
 
     fun get(selector: String) : SelenideElement {
-        return `$`(selector);
+        return `$`(selector)
     }
     
     fun all(selector: String) : ElementsCollection {
-        return `$$`(selector);
+        return `$$`(selector)
     }
 }
