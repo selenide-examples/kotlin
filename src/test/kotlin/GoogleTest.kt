feed9ab17cf64c6a1cf17d31d6962d2aa528a7c2
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-10-08 00:47:16 +0300

    update google test

--- src/test/kotlin/GoogleTest.kt
+++ src/test/kotlin/GoogleTest.kt
@@ -2,14 +2,9 @@ import com.codeborne.selenide.CollectionCondition.sizeGreaterThan
 import com.codeborne.selenide.Condition.text
 import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.ElementsCollection
-import com.codeborne.selenide.Selenide.open
-import com.codeborne.selenide.Selenide.`$`
-import com.codeborne.selenide.Selenide.`$$`
-import com.codeborne.selenide.Selenide.element
-import com.codeborne.selenide.Selenide.elements
+import com.codeborne.selenide.Selenide.*
 import com.codeborne.selenide.SelenideElement
 import org.junit.jupiter.api.BeforeAll
-
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.openqa.selenium.By
@@ -31,20 +26,20 @@ class GoogleTest {
     @Test
     fun usingDollarsWithBackticks() {
         `$`(By.name("q")).setValue("selenide").pressEnter()
-        `$$`("#ires .g").shouldHave(sizeGreaterThan(5))
-        `$`("#ires .g").shouldHave(text("concise ui tests in Java"))
+        `$$`("#res .g").shouldHave(sizeGreaterThan(5))
+        `$`("#res .g").shouldHave(text("concise ui tests in Java"))
     }
 
     @Test fun notUsingDollars() {
         element(By.name("q")).setValue("selenide").pressEnter()
-        elements(By.cssSelector("#ires .g")).shouldHave(sizeGreaterThan(5))
-        element(By.cssSelector("#ires .g")).shouldHave(text("concise ui tests in Java"))
+        elements(By.cssSelector("#res .g")).shouldHave(sizeGreaterThan(5))
+        element(By.cssSelector("#res .g")).shouldHave(text("concise ui tests in Java"))
     }
 
     @Test fun usingAliases() {
         get("[name=q]").setValue("selenide").pressEnter()
-        all("#ires .g").shouldHave(sizeGreaterThan(5))
-        get("#ires .g").shouldHave(text("concise ui tests in Java"))
+        all("#res .g").shouldHave(sizeGreaterThan(5))
+        get("#res .g").shouldHave(text("concise ui tests in Java"))
     }
 
     fun get(selector: String) : SelenideElement {
